4c3bdfdaac5bec8cf8fc7bc052a6c406ef3b64be
ADARSH KUMAR GUPTA <106335949+AdarshGzz@users.noreply.github.com>
2023-02-22 00:19:53 +0530

    learned about primitive types and instances in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,50 @@
-let message : string = 'Hello World';
-console.log(message);
+///////// Vedio 2 ///////////
+
+// let BooleanType : boolean = true;
+// let NumberType : number = 100;
+// let StringType : string = "Adarsh";
+
+// let UndefinedType : undefined = undefined;
+// let NullType : null = null;
+
+// let SymbolType : symbol = Symbol('star');
+// let BigIntegerType : bigint = 24n;
+
+///////// Vedio 3 ///////////
+
+// let regexp: RegExp = new RegExp('ab+c');
+
+// let array: Array<number> = [1, 2, 3, 4];
+
+// let set: Set<number> = new Set([1, 2, 3, 4, 4])
+
+// // A first in first out collection
+
+// class Queue<T> {
+//     private data: Array<T> = [];
+//     push(item: T) { this.data.push(item); }
+//     pop(): T | undefined { return this.data.shift(); }
+// }
+
+// let queue: Queue<number> = new Queue();
+
+///////// Vedio 4 ///////////
+// ////Arrays
+// let array : Array<number> = [1, 2, 3, 4];
+// // anothe short way of defining an array
+// let array1 : number[] = [1, 2, 3, 4];
+
+// //usage
+// array = [1];
+// array = [1,2,3,4,5,6];
+// array = ['name'] //Error: this is array of numbers
+
+////Tupple
+
+// let tupple : [number,number] = [5,6]
+
+// //usage
+// tupple = [1] // Error: must be 2 items 
+// tupple = [2,5]
+// tupple = [1,1]
+// tupple = [2,4,5] // Error: must be only two items
